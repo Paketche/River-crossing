23591defcf02aedcb895a3f37f3224a0036333d5
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2017-05-23 12:13:07 +0100

    Update TileButton class:
    Added comments.

--- src/tile/TileButton.java
+++ src/tile/TileButton.java
@@ -11,33 +11,39 @@ class TileButton extends JButton{
 
 	private static final long serialVersionUID = 1L;
 
+	// location of the button in the tile manager that is going to be placed in
 	public final Point location;
+	// The tile that the button holds 
 	private Tile tile;
 	
-
-	public TileButton(TileManager tm, Tile t, Point p){
+	// constructor for TileManager
+	TileButton(TileManager tm, Tile t, Point p){
 		super(t.icon);
 		tile = t;
 		location = p;
-		this.setPreferredSize(new Dimension(32,32));
-		//setBorderPainted(false);
-		addActionListener(new ActionListener(){
+		this.setPreferredSize(new Dimension(32, 32));
+		setBorderPainted(false);
+		addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
 				tm.act(location);
 			}
 		});
 	}
-	public TileButton(Tile t,Point p){
+	
+	//Constructor for TileHolder
+	TileButton(Tile t, Point p){
 		super(t.icon);
-		tile =t;
+		tile = t;
 		location = p;
 	}
-	
+
+	// Changes the tile and the icon of the button
 	void setTile(Tile tile){
 		this.tile = tile;
 		setIcon(tile.icon);
 	}
 	
+	// return the reference of the tile
 	Tile getTile(){
 		return tile;
 	}
