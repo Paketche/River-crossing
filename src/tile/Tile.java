8a99ab89c732de94cb5691f1e1a0aa2482c7a3b6
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2017-05-23 11:25:10 +0100

    Update Tile class:
    Made the class abstract. Added comments.

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -2,7 +2,7 @@ package tile;
 
 import javax.swing.ImageIcon;
 
-abstract class Tile{
+public abstract class Tile{
 
 	public static final int WATER = 0;
 	public static final int BANK1 = 1;
@@ -20,53 +20,73 @@ abstract class Tile{
 	public static final int FISH1 = 13;
 	public static final int FISH2 = 14;
 	public static final int FISH3 = 15;
+	
+	final int identifier;// holds a value that helps identify what the tile is(WATER, PLANK, STUMP etc.)
+	final ImageIcon icon;//holds the image icon
+	private TileButton tileButton;// reference to the tile button that this tile is held in
 
-	public final int identifier;
-	public final int priority;
-	public final int priorityHierarchy;
-	public final ImageIcon icon;
-
-	// private boolean accessible;
-	// private boolean isAPath;
-
-	protected Tile(ImageIcon icon, int i, int pH, int p){
+	// creates a tile object and sets an icon and an identifier
+	protected Tile(ImageIcon icon, int i){
 		this.icon = icon;
 		identifier = i;
-		priorityHierarchy = pH;
-		priority = p;
 	}
 
+	/** 
+	 * set the reference to the tile button that this tile is held in
+	 * @param tileButton
+	 */
+	public void setTileButton(TileButton tileButton){
+		this.tileButton = tileButton;
+	}
+
+	/** 
+	 * returns the reference to the tile button that this tile is held in
+	 * @return
+	 */
+	public TileButton getTileButton(){
+		return this.tileButton;
+	}
+
+	/**
+	 * Constructs and returns a tile object.
+	 * 
+	 * Use one of the static constants of the class to get a tile with
+	 * corresponding attributes and icon. The method returns: WFB, Plank, Post.
+	 * All of the classes returned are subclasses of Tile and hold their special
+	 * features.
+	 * 
+	 * @param ident
+	 * @return
+	 */
 	public static Tile getTile(int ident){
 		Tile t;
-		
 		switch (ident) {
-			case 0:
-			case 1:
-			case 2:
-			case 13:
-			case 14:
-			case 15:
+			case Tile.WATER:
+			case Tile.BANK1:
+			case Tile.BANK2:
+			case Tile.FISH1:
+			case Tile.FISH2:
+			case Tile.FISH3:
 				t = new WFB(ident);
 				break;
-			case 3:
-			case 4:
-			case 5:
-			case 6:
+			case Tile.PLANK1:
+			case Tile.PLANK1_MAN:
+			case Tile.PLANK2:
+			case Tile.PLANK2_MAN:
 				t = new Plank(ident);
 				break;
-			case 7:
-			case 8:
-			case 9:
-			case 10:
-			case 11:
-			case 12:
+			case Tile.STUMP1:
+			case Tile.STUMP1_MAN:
+			case Tile.STUMP2:
+			case Tile.STUMP2_MAN:
+			case Tile.STUMP3:
+			case Tile.STUMP3_MAN:
 				t = new Post(ident);
 				break;
-			default: 
-				t = new WFB(ident);
-			
+			default:
+				t = new Post(ident);
 		}
-	
+
 		return t;
 	}
 
