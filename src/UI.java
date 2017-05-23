4d79dab3771e6f42961bb207bb701f13d185e59d
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2017-05-23 23:50:51 +0100

    Update UI class:
    High scores have names. Text field for entering new names.

--- src/UI.java
+++ src/UI.java
@@ -7,6 +7,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import tile.*;
 
 public class UI extends JFrame{
@@ -16,27 +18,34 @@ public class UI extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JPanel northPanel = new JPanel();// panel for the high and current scores 
-	private JPanel eastPanel = new JPanel(new BorderLayout());// panel for the level buttons and inventory
-	private JPanel startp = new JPanel();//panel for the level buttons (goes in eastPanel)
+	private JPanel northPanel = new JPanel();// panel for the high and current
+																						// scores
+	private JPanel eastPanel = new JPanel(new BorderLayout());// panel for the
+																														// level buttons and
+																														// inventory
+	private JPanel southPanel = new JPanel(new BorderLayout());
+	private JPanel startp = new JPanel();// panel for the level buttons (goes in
+																				// eastPanel)
 	public final TileManager tileManager;
-	//buttons for the levels
+	// buttons for the levels
 	private JButton lvl1;
 	private JButton lvl2;
 	private JButton lvl3;
-	
-	
+	private JTextField textField;
+
 	// stop watch to count the time passed
 	private StopWatch sW = new StopWatch();
 
 	// holds the number for the current level
-	int level;
-	
+	private int level;
+
+	private String[] names = { "Jeff", "John", "Cena" };
 	// holds the scores for the different levels
 	private int[] highestScore = { 100, 75, 50 };
 
 	private JLabel highestS = new JLabel();
 	private JLabel timer = new JLabel();
+	private JLabel enterName = new JLabel("Enter your name here:");
 
 	private boolean stopTimer = false;
 
@@ -49,12 +58,12 @@ public class UI extends JFrame{
 		// synchronized (frame){
 		while (true){
 			// if the game has been won we want to stop updating the time on the ui
-			if (!frame.getTimerState())
+			if (!frame.getTimerState()&&!frame.tileManager.isWon())
 				frame.getTimer().setText("Time: " + frame.getSWread());
 			// if the has been won and there is a new high score we want to change it
 			if (frame.tileManager.isWon()){
-				frame.newScore();
-				frame.tileManager.unWin();// we want to set the new score only once; and
+				frame.stopTimer();
+				// we want to set the new score only once; and
 																	// this stops a bug where when you start the
 																	// game again your high score is 0
 			}
@@ -67,12 +76,12 @@ public class UI extends JFrame{
 	public UI(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setSize(360, 460);
-		//set content pane
+		setSize(360, 480);
+		// set content pane
 		contentPane = new JPanel(new BorderLayout());
 		setContentPane(contentPane);
-		
-		//give an initial level to the ui and add it
+
+		// give an initial level to the ui and add it
 		tileManager = new TileManager(new Level(2));
 		level = tileManager.getLevel();
 		contentPane.add(tileManager, BorderLayout.CENTER);
@@ -81,17 +90,7 @@ public class UI extends JFrame{
 		lvl1 = new JButton("Level 1");
 		lvl1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				level = 1;
-				sW.start();// start the watch
-				
-				tileManager.loadNewLevel(new Level(1));
-				level = tileManager.getLevel();
-				synchronized (this){
-					stopTimer = false;
-				}
-
-				// set the highest score for the corresponding level
-				highestS.setText("Fastest run: " + Integer.toString(highestScore[level - 1]) + " sec   ");
+				buttonFunction(1);
 			}
 		});
 		startp.add(lvl1);
@@ -100,56 +99,69 @@ public class UI extends JFrame{
 		lvl2 = new JButton("Level 2");
 		lvl2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				level = 2;
-				sW.start();// start the watch
-
-				tileManager.loadNewLevel(new Level(2));
-				level = tileManager.getLevel();
-				synchronized (this){
-					stopTimer = false;
-				}
-
-				// set the highest score for the corresponding level
-				highestS.setText("Fastest run: " + Integer.toString(highestScore[level - 1]) + " sec   ");
+				buttonFunction(2);
 			}
 		});
 		startp.add(lvl2);
-		/*
-		 * //adding the button for the second level lvl3 = new JButton("Level 3");
-		 * lvl3.addActionListener(new ActionListener() { public void
-		 * actionPerformed(ActionEvent e){ sW.start();
-		 * 
-		 * tileManager.loadNewLevel(new Level(30)); synchronized (this){ stopTimer =
-		 * false; } highestS.setText("Fastest run: "+
-		 * Integer.toString(highestScore)+" sec   "); } }); startp.add(lvl3);
-		 */
+
+		// adding the button for the third level
+		lvl3 = new JButton("Level 3");
+		lvl3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				buttonFunction(3);
+			}
+		});
+		startp.add(lvl3);
+
 		startp.setPreferredSize(new Dimension(64, 150));
 		eastPanel.add(startp, BorderLayout.CENTER);
 		// add the labels for the highest score
 		northPanel.add(highestS);
 		northPanel.add(timer);
 
-		// inventory = tileManager.inventory;
-		// inventory.setPreferredSize(new Dimension(64,66));
 		eastPanel.add(tileManager.inventory, BorderLayout.SOUTH);
+
+		textField = new JTextField();
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				if (tileManager.isWon())
+					newScore();
+			}
+		});
+		southPanel.add(enterName,BorderLayout.WEST);
+		southPanel.add(textField, BorderLayout.CENTER);
+
+		contentPane.add(southPanel, BorderLayout.SOUTH);
 		contentPane.add(eastPanel, BorderLayout.EAST);
 		contentPane.add(northPanel, BorderLayout.NORTH);
+		highestS.setText("Fastest run: " + Integer.toString(highestScore[level - 1]) + " secs by " + names[level - 1]);
 		sW.start();
 	}
 
 	public void newScore(){
+
 		if (sW.getCurrentTime() < highestScore[level - 1]){
 			highestScore[level - 1] = sW.getCurrentTime();
+			names[level - 1] = textField.getText();
 		}
-		highestS.setText("Fastest run: " + Integer.toString(highestScore[level - 1]) + "sec   ");
-		synchronized (this){
-			stopTimer = true;
-		}
+		highestS.setText("Fastest run: " + Integer.toString(highestScore[level - 1]) + " secs by " + names[level - 1]);
+		stopTimer = true;
+		tileManager.unWin();
 	}
 
 	public String getSWread(){
-		int seconds = (sW.getCurrentTime());
-		return Integer.toString(seconds);
+		return Integer.toString(sW.getCurrentTime());
+	}
+
+	private void buttonFunction(int i){
+		sW.start();// start the watch
+		tileManager.loadNewLevel(new Level(i));
+		level = tileManager.getLevel();
+		synchronized (this){
+			stopTimer = false;
+		}
+		// set the highest score for the corresponding level
+		highestS.setText("Fastest run: " + Integer.toString(highestScore[level - 1]) + " secs by " + names[level - 1]);
 	}
 
 	public JLabel getTimer(){
@@ -158,5 +170,9 @@ public class UI extends JFrame{
 
 	public boolean getTimerState(){
 		return stopTimer;
+	}
+
+	public void stopTimer(){
+		stopTimer = true;
 	}
 }
